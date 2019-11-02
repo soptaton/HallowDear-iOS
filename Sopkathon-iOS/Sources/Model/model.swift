a7bda4341d00695cf841c6eacc5228f46af32db8
Seungsoo Lee <soogoon.io@gmail.com>
2019-11-02 22:40:27 +0900

    feat: Init project

---
+++ Sopkathon-iOS/Sources/Model/model.swift
@@ -0,0 +1,11 @@
+//
+//  model.swift
+//  Sopkathon-iOS
+//
+//  Created by LeeSeungsoo on 2019/11/02.
+//  Copyright © 2019 SOPT. All rights reserved.
+//
+
+import Foundation
+
+// Git 공유를 위한 가짜 파일
---
+++ Sopkathon-iOS/Sources/Networking/APIServices/APIService.swift
@@ -0,0 +1,11 @@
+//
+//  APIService.swift
+//  Sopkathon-iOS
+//
+//  Created by LeeSeungsoo on 2019/11/02.
+//  Copyright © 2019 SOPT. All rights reserved.
+//
+
+import Foundation
+
+// API service
